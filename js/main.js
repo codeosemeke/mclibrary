8a304baa9a70ce3ef027376b381fc354394f9580
Osemeke Samuel <samuel@tedbree.com>
2021-06-24 03:00:05 +0100

    Mobile Menu JS

--- js/main.js
+++ js/main.js
@@ -2,24 +2,34 @@ if(document.getElementById('mobile_menu_toggle_button') !='undefined'){
     let mobile_menu_button = document.getElementById('mobile_menu_toggle_button');
 
     mobile_menu_button.addEventListener('click', function (e) {
-        let mobile_menu = document.getElementById('mobile-menu');
-        let menu_burger = document.getElementById('menu-burger');
-        let menu_x = document.getElementById('menu-x');
-        toggleElementClass(mobile_menu, 'hidden')
-        toggleElementClass(menu_burger, 'hidden')
-        toggleElementClass(menu_x, 'hidden')
-    })
-    
+        document.getElementById('mobile-menu').classList.toggle("hidden");
+        document.getElementById('menu-burger').classList.toggle("hidden");
+        document.getElementById('menu-x').classList.toggle("hidden");
+    })  
 }
 
+if(document.querySelectorAll('.accordion').length > 0){
+    var acc = document.querySelectorAll(".accordion");
+    var i;
 
-// Function to toggle a class
-function toggleElementClass(element, elemClass) {
-    if(element.classList.contains(elemClass)){
-        element.classList.remove(elemClass) 
-    }
-    else{
-        element.classList.add(elemClass) 
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            // return console.log(i, this.parentElement.nextElementSibling)
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            // this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.parentElement.nextElementSibling;
+            // var panel = this.nextElementSibling;
+
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
     }
 }
+
 
